39171b0683283c8b05bce62738d921a1e186c223
manucastelnovo <marnutskate@gmail.com>
2025-01-24 18:49:01 -0300

    chore: add axios instance for API communication

---
+++ src/lib/http.ts
@@ -0,0 +1,15 @@
+
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+
+const nestApi = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export {
+    nestApi
+}
