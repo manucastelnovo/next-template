d68e9daac1d84064d260f3fa164a1a3cc1e2cd2b
manucastelnovo <marnutskate@gmail.com>
2025-01-24 18:49:37 -0300

    chore: update import paths in revenue-chart component to reflect new directory structure

--- src/ui/dashboard/revenue-chart.tsx
+++ src/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
-import { generateYAxis } from "@/app/lib/utils";
+import { generateYAxis } from "@/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
-import { Revenue } from "@/app/lib/definitions";
+import { Revenue } from "@/types/definitions";
 
 // This component is representational only.
 // For data visualization UI, check out:
