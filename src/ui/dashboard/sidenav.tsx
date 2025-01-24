f0512039fbe26147f699bf14f984136af3ef8a16
manucastelnovo <marnutskate@gmail.com>
2025-01-24 18:49:40 -0300

    chore: update sidenav component to include logout functionality and improve form handling

--- src/ui/dashboard/sidenav.tsx
+++ src/ui/dashboard/sidenav.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { PowerIcon, Cog8ToothIcon } from "@heroicons/react/24/outline";
 import NavLinks from "./nav-links";
 import AcmeLogo from "../acme-logo";
+import { logout } from "@/lib/session";
 
 export default function SideNav() {
   return (
@@ -18,12 +19,24 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-mainColor md:block"></div>
-        <form className="flex flex-row md:flex-col">
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-mainColor p-3 text-sm font-medium hover:bg-orange-100 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3">
+        <form
+          className="flex flex-row md:flex-col"
+          action={async () => {
+            "use server";
+            await logout();
+          }}
+        >
+          <Link
+            href="/profile"
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-mainColor p-3 text-sm font-medium hover:bg-orange-100 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3"
+          >
             <Cog8ToothIcon className="w-6" />
             <div className="hidden md:block">Mi Perfil</div>
-          </button>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-mainColor p-3 text-sm font-medium hover:bg-orange-100 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3">
+          </Link>
+          <button
+            type="submit"
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-mainColor p-3 text-sm font-medium hover:bg-orange-100 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3"
+          >
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Cerrar sesion</div>
           </button>
